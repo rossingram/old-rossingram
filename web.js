90282fbe9626375ed871a42b3fe1db0e838deec6
Ross Ingram <rosingram@gmail.com>
2012-07-14 22:42:09 -0600

    changes

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ app.get('/', function(request, response) {
   response.render("index.ejs", {layout:false});
 });
 app.use('/static', express.static(__dirname + '/static')); 
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
